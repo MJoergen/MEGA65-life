bd9454c3edbf35961228f2c31b47100868d8f99f
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2024-08-12 22:10:40 +0200

    Add keyboard repeat
    This is part of issue #1

--- CORE/vhdl/controller/controller_wrapper.vhd
+++ CORE/vhdl/controller/controller_wrapper.vhd
@@ -49,87 +49,89 @@ architecture synthesis of controller_wrapper is
 
    -- MEGA65 key codes that kb_key_num_i is using while
    -- kb_key_pressed_n_i is signalling (low active) which key is pressed
-   constant C_M65_INS_DEL     : integer                          := 0;
-   constant C_M65_RETURN      : integer                          := 1;
-   constant C_M65_HORZ_CRSR   : integer                          := 2;  -- means cursor right in C64 terminology
-   constant C_M65_F7          : integer                          := 3;
-   constant C_M65_F1          : integer                          := 4;
-   constant C_M65_F3          : integer                          := 5;
-   constant C_M65_F5          : integer                          := 6;
-   constant C_M65_VERT_CRSR   : integer                          := 7;  -- means cursor down in C64 terminology
-   constant C_M65_3           : integer                          := 8;
-   constant C_M65_W           : integer                          := 9;
-   constant C_M65_A           : integer                          := 10;
-   constant C_M65_4           : integer                          := 11;
-   constant C_M65_Z           : integer                          := 12;
-   constant C_M65_S           : integer                          := 13;
-   constant C_M65_E           : integer                          := 14;
-   constant C_M65_LEFT_SHIFT  : integer                          := 15;
-   constant C_M65_5           : integer                          := 16;
-   constant C_M65_R           : integer                          := 17;
-   constant C_M65_D           : integer                          := 18;
-   constant C_M65_6           : integer                          := 19;
-   constant C_M65_C           : integer                          := 20;
-   constant C_M65_F           : integer                          := 21;
-   constant C_M65_T           : integer                          := 22;
-   constant C_M65_X           : integer                          := 23;
-   constant C_M65_7           : integer                          := 24;
-   constant C_M65_Y           : integer                          := 25;
-   constant C_M65_G           : integer                          := 26;
-   constant C_M65_8           : integer                          := 27;
-   constant C_M65_B           : integer                          := 28;
-   constant C_M65_H           : integer                          := 29;
-   constant C_M65_U           : integer                          := 30;
-   constant C_M65_V           : integer                          := 31;
-   constant C_M65_9           : integer                          := 32;
-   constant C_M65_I           : integer                          := 33;
-   constant C_M65_J           : integer                          := 34;
-   constant C_M65_0           : integer                          := 35;
-   constant C_M65_M           : integer                          := 36;
-   constant C_M65_K           : integer                          := 37;
-   constant C_M65_O           : integer                          := 38;
-   constant C_M65_N           : integer                          := 39;
-   constant C_M65_PLUS        : integer                          := 40;
-   constant C_M65_P           : integer                          := 41;
-   constant C_M65_L           : integer                          := 42;
-   constant C_M65_MINUS       : integer                          := 43;
-   constant C_M65_DOT         : integer                          := 44;
-   constant C_M65_COLON       : integer                          := 45;
-   constant C_M65_AT          : integer                          := 46;
-   constant C_M65_COMMA       : integer                          := 47;
-   constant C_M65_GBP         : integer                          := 48;
-   constant C_M65_ASTERISK    : integer                          := 49;
-   constant C_M65_SEMICOLON   : integer                          := 50;
-   constant C_M65_CLR_HOME    : integer                          := 51;
-   constant C_M65_RIGHT_SHIFT : integer                          := 52;
-   constant C_M65_EQUAL       : integer                          := 53;
-   constant C_M65_ARROW_UP    : integer                          := 54; -- symbol, not cursor
-   constant C_M65_SLASH       : integer                          := 55;
-   constant C_M65_1           : integer                          := 56;
-   constant C_M65_ARROW_LEFT  : integer                          := 57; -- symbol, not cursor
-   constant C_M65_CTRL        : integer                          := 58;
-   constant C_M65_2           : integer                          := 59;
-   constant C_M65_SPACE       : integer                          := 60;
-   constant C_M65_MEGA        : integer                          := 61;
-   constant C_M65_Q           : integer                          := 62;
-   constant C_M65_RUN_STOP    : integer                          := 63;
-   constant C_M65_NO_SCRL     : integer                          := 64;
-   constant C_M65_TAB         : integer                          := 65;
-   constant C_M65_ALT         : integer                          := 66;
-   constant C_M65_HELP        : integer                          := 67;
-   constant C_M65_F9          : integer                          := 68;
-   constant C_M65_F11         : integer                          := 69;
-   constant C_M65_F13         : integer                          := 70;
-   constant C_M65_ESC         : integer                          := 71;
-   constant C_M65_CAPSLOCK    : integer                          := 72;
-   constant C_M65_UP_CRSR     : integer                          := 73; -- cursor up
-   constant C_M65_LEFT_CRSR   : integer                          := 74; -- cursor left
-   constant C_M65_RESTORE     : integer                          := 75;
-   constant C_M65_NONE        : integer                          := 79;
+   constant C_M65_INS_DEL     : integer          := 0;
+   constant C_M65_RETURN      : integer          := 1;
+   constant C_M65_HORZ_CRSR   : integer          := 2;  -- means cursor right in C64 terminology
+   constant C_M65_F7          : integer          := 3;
+   constant C_M65_F1          : integer          := 4;
+   constant C_M65_F3          : integer          := 5;
+   constant C_M65_F5          : integer          := 6;
+   constant C_M65_VERT_CRSR   : integer          := 7;  -- means cursor down in C64 terminology
+   constant C_M65_3           : integer          := 8;
+   constant C_M65_W           : integer          := 9;
+   constant C_M65_A           : integer          := 10;
+   constant C_M65_4           : integer          := 11;
+   constant C_M65_Z           : integer          := 12;
+   constant C_M65_S           : integer          := 13;
+   constant C_M65_E           : integer          := 14;
+   constant C_M65_LEFT_SHIFT  : integer          := 15;
+   constant C_M65_5           : integer          := 16;
+   constant C_M65_R           : integer          := 17;
+   constant C_M65_D           : integer          := 18;
+   constant C_M65_6           : integer          := 19;
+   constant C_M65_C           : integer          := 20;
+   constant C_M65_F           : integer          := 21;
+   constant C_M65_T           : integer          := 22;
+   constant C_M65_X           : integer          := 23;
+   constant C_M65_7           : integer          := 24;
+   constant C_M65_Y           : integer          := 25;
+   constant C_M65_G           : integer          := 26;
+   constant C_M65_8           : integer          := 27;
+   constant C_M65_B           : integer          := 28;
+   constant C_M65_H           : integer          := 29;
+   constant C_M65_U           : integer          := 30;
+   constant C_M65_V           : integer          := 31;
+   constant C_M65_9           : integer          := 32;
+   constant C_M65_I           : integer          := 33;
+   constant C_M65_J           : integer          := 34;
+   constant C_M65_0           : integer          := 35;
+   constant C_M65_M           : integer          := 36;
+   constant C_M65_K           : integer          := 37;
+   constant C_M65_O           : integer          := 38;
+   constant C_M65_N           : integer          := 39;
+   constant C_M65_PLUS        : integer          := 40;
+   constant C_M65_P           : integer          := 41;
+   constant C_M65_L           : integer          := 42;
+   constant C_M65_MINUS       : integer          := 43;
+   constant C_M65_DOT         : integer          := 44;
+   constant C_M65_COLON       : integer          := 45;
+   constant C_M65_AT          : integer          := 46;
+   constant C_M65_COMMA       : integer          := 47;
+   constant C_M65_GBP         : integer          := 48;
+   constant C_M65_ASTERISK    : integer          := 49;
+   constant C_M65_SEMICOLON   : integer          := 50;
+   constant C_M65_CLR_HOME    : integer          := 51;
+   constant C_M65_RIGHT_SHIFT : integer          := 52;
+   constant C_M65_EQUAL       : integer          := 53;
+   constant C_M65_ARROW_UP    : integer          := 54; -- symbol, not cursor
+   constant C_M65_SLASH       : integer          := 55;
+   constant C_M65_1           : integer          := 56;
+   constant C_M65_ARROW_LEFT  : integer          := 57; -- symbol, not cursor
+   constant C_M65_CTRL        : integer          := 58;
+   constant C_M65_2           : integer          := 59;
+   constant C_M65_SPACE       : integer          := 60;
+   constant C_M65_MEGA        : integer          := 61;
+   constant C_M65_Q           : integer          := 62;
+   constant C_M65_RUN_STOP    : integer          := 63;
+   constant C_M65_NO_SCRL     : integer          := 64;
+   constant C_M65_TAB         : integer          := 65;
+   constant C_M65_ALT         : integer          := 66;
+   constant C_M65_HELP        : integer          := 67;
+   constant C_M65_F9          : integer          := 68;
+   constant C_M65_F11         : integer          := 69;
+   constant C_M65_F13         : integer          := 70;
+   constant C_M65_ESC         : integer          := 71;
+   constant C_M65_CAPSLOCK    : integer          := 72;
+   constant C_M65_UP_CRSR     : integer          := 73; -- cursor up
+   constant C_M65_LEFT_CRSR   : integer          := 74; -- cursor left
+   constant C_M65_RESTORE     : integer          := 75;
+   constant C_M65_NONE        : integer          := 79;
 
-   signal   main_key_num      : integer range 0 to 79;
-   signal   main_key_pressed  : std_logic;
-   signal   main_key_released : std_logic;
+   type     main_key_state_type is (UP_ST, DOWN_ST);
+   signal   main_key_state : main_key_state_type := UP_ST;
+   signal   main_key_timer : integer range 0 to G_MAIN_CLK_HZ;
+   signal   main_key_num   : integer range 0 to 79;
+   signal   main_key_valid : std_logic;
 
    signal   main_uart_rx_ready : std_logic;
    signal   main_uart_rx_valid : std_logic;
@@ -168,26 +170,47 @@ begin
    key_proc : process (main_clk_i)
    begin
       if rising_edge(main_clk_i) then
-         main_key_pressed <= '0';
+         main_key_valid <= '0';
 
-         if main_kb_key_pressed_n_i = '0' then
-            if main_key_num /= main_kb_key_num_i or main_key_released = '1' then
-               main_key_num      <= main_kb_key_num_i;
-               main_key_pressed  <= '1';
-               main_key_released <= '0';
-            end if;
-         end if;
+         case main_key_state is
 
-         if main_kb_key_pressed_n_i = '1' then
-            if main_key_num = main_kb_key_num_i then
-               main_key_released <= '1';
-            end if;
-         end if;
+            when UP_ST =>
+               -- A key is pressed
+               if main_kb_key_pressed_n_i = '0' then
+                  main_key_num   <= main_kb_key_num_i;
+                  main_key_valid <= '1';
+                  main_key_timer <= G_MAIN_CLK_HZ;
+                  main_key_state <= DOWN_ST;
+               end if;
+
+            when DOWN_ST =>
+               -- A different key is pressed
+               if main_kb_key_pressed_n_i = '0' and main_key_num /= main_kb_key_num_i then
+                  main_key_num   <= main_kb_key_num_i;
+                  main_key_valid <= '1';
+                  main_key_timer <= G_MAIN_CLK_HZ;
+               end if;
+
+               -- The key is released
+               if main_kb_key_pressed_n_i = '1' and main_key_num = main_kb_key_num_i then
+                  main_key_state <= UP_ST;
+               else
+                  -- The key is still pressed
+                  if main_key_timer > 0 then
+                     main_key_timer <= main_key_timer - 1;
+                  else
+                     main_key_valid <= '1';
+                     main_key_timer <= G_MAIN_CLK_HZ / 5;
+                  end if;
+               end if;
+
+         end case;
 
          if main_rst_i = '1' then
-            main_key_pressed  <= '0';
-            main_key_released <= '1';
-            main_key_num      <= C_M65_NONE;
+            main_key_state <= UP_ST;
+            main_key_timer <= G_MAIN_CLK_HZ;
+            main_key_num   <= C_M65_NONE;
+            main_key_valid <= '0';
          end if;
       end if;
    end process key_proc;
@@ -228,7 +251,7 @@ begin
             end if;
          end if;
 
-         if main_key_pressed = '1' then
+         if main_key_valid = '1' then
             main_cmd_valid <= '1';
 
             case main_key_num is
